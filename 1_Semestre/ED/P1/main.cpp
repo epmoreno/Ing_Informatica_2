10cf320359b074ab6b429fb106a7b65735296b62
epmoreno <enriquepinazomoreno20001@hotmail.com>
2023-09-29 21:57:05 +0200

    P1_ED
    Apuntes e inicio de practica nº_1

---
+++ 1_Semestre/ED/P1/main.cpp
@@ -0,0 +1,9 @@
+void ordenar(int *v, int n) {
+    for (int i=0; i<n-1; i++)
+        for (int j=0; j<n-i-1; j++)
+            if (v[j]>v[j+1]) {
+                int aux = v[j];
+                v[j] = v[j+1];
+                v[j+1] = aux;
+            }
+}

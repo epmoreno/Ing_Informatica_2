9b619a9ba97c79e8693a84fc65d56fd565f43302
epmoreno <enriquepinazomoreno20001@hotmail.com>
2023-10-03 15:05:10 +0200

    Ejercicio1_P1

--- 1_Semestre/ED/P1/ordenacion.cpp
+++ 1_Semestre/ED/P1/ordenacion.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <ctime> // Recursos para medir tiempos
+#include <cstdlib> // Para generación de números pseudoaleatorios
+using namespace std;
+
 void ordenar(int *v, int n) {
     for (int i=0; i<n-1; i++)
         for (int j=0; j<n-i-1; j++)
@@ -7,8 +12,34 @@ void ordenar(int *v, int n) {
                 v[j+1] = aux;
             }
 }
-int main(int argc, char const *argv[])
-{
-    /* code */
+
+
+int main()
+{   
+    //GENERADOR DE VECTOR ALEATORIO
+    int tam = 30000;
+    int *v=new int[tam];
+    srand(time(0)); //inicialización generador numeros pseudoaleatorios
+    for (int i = 0; i < tam; i++){
+        v[i] = rand() & RAND_MAX;
+    }
+    
+
+     
+    //CALCULO DEL TIEMPO
+    clock_t tini ;//tiempo de inicio
+    tini=clock();
+
+    ordenar(v, tam);//ORDENAMOS
+
+    clock_t tfin ;//tiempo de finalizacion
+    tfin=clock();
+    
+    cout<<endl;
+    cout<<"Tamaño = "<<tam<<", tiempo de ejecución en seg = "<<(tfin-tini)/(double)CLOCKS_PER_SEC<<endl;
+    cout<<endl;
+    
+    // Liberar la memoria asignada al array
+    delete[] v;
     return 0;
 }

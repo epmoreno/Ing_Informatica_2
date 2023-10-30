c2807fa58af2bb27e3a53d1774366847a403c999
epmoreno <enriquepinazomoreno20001@hotmail.com>
2023-10-30 12:02:44 +0100

    101%

--- 1_Semestre/ED/P2/cronologia/doc/html/search/files_2.js
+++ 1_Semestre/ED/P2/cronologia/doc/html/search/files_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['main_2ecpp_0',['main.cpp',['../main_8cpp.html',1,'']]],
-  ['makefile_1',['makefile',['../latex_2Makefile.html',1,'(Espacio de nombres global)'],['../Makefile.html',1,'(Espacio de nombres global)']]],
+  ['makefile_1',['Makefile',['../Makefile.html',1,'']]],
   ['makefile_2emd_2',['Makefile.md',['../Makefile_8md.html',1,'']]]
 ];
